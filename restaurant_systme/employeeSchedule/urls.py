bc27ec3ddba44096803079dad4733fa108099439
xxmu53xx <73223072+xxmu53xx@users.noreply.github.com>
2024-10-28 13:03:05 +0800

    added update/delete

--- restaurant_systme/employeeSchedule/urls.py
+++ restaurant_systme/employeeSchedule/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     path('Dddisplay/',views.display_schedule,name='employeeSDisplay'),
     path('',views.display_schedulev2,name='display'),
     path('add-schedule/', views.add_schedule, name='add_schedule'),
+     path('get-schedule/<int:schedule_id>/', views.get_schedule, name='get_schedule'),
+    path('delete-schedule/<int:schedule_id>/', views.delete_schedule, name='delete_schedule'),
+    path('edit-schedule/',views.update_schedule,name='update_schedule'),
+    path('update_schedule/<int:schedule_id>', views.update_schedule, name='update_schedule'),
+   path('employeeSchedule/delete-schedule/<int:schedule_id>', views.delete_schedule, name='delete_schedule'),
 ]
